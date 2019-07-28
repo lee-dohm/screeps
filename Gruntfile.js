4d44b71e1e9faba4388978e147c13058f992888d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-28 15:43:49 -0700

    Add the ability to override grunt screeps configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function(grunt) {
-  var config = require('./.screeps.json');
+  var config = require('./.screeps.json')
+
+  var branch = grunt.option('branch') || config.branch
+  var email = grunt.option('email') || config.email
+  var password = grunt.option('password') || config.password
+  var ptr = grunt.option('ptr') ? true : config.ptr
 
   grunt.loadNpmTasks('grunt-screeps');
 
