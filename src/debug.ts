9286218071ea42905270da6466eaa169efa9f1e7
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-28 13:22:13 -0700

    Keep a short history of the amount of used CPU time

--- src/debug.ts
+++ src/debug.ts
@@ -5,9 +5,26 @@ export function log(message: string) {
 }
 
 export function logStats() {
+  const percentUsed = Math.round((Game.cpu.getUsed() / Game.cpu.tickLimit) * 100)
+
   if (Memory.debug || Memory.stats) {
     console.log(
       `${Math.round((Game.cpu.getUsed() / Game.cpu.tickLimit) * 100)}% of available CPU time used`
     )
+    console.log(`History: ${JSON.stringify(Memory.usedHistory)}`)
+  }
+
+  updateUsedHistory(percentUsed)
+}
+
+function updateUsedHistory(used: number) {
+  if (Memory.usedHistory) {
+    while (Memory.usedHistory.length > 4) {
+      Memory.usedHistory.shift()
+    }
+
+    Memory.usedHistory.push(used)
+  } else {
+    Memory.usedHistory = [used]
   }
 }
