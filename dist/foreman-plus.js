0d86a1def3f858b88bcc941680aa970c409077d8
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-31 19:19:09 -0700

    :fire: All unnecessary code in ForemanPlus

--- dist/foreman-plus.js
+++ dist/foreman-plus.js
@@ -8,21 +8,6 @@ class ForemanPlus {
     }
   }
 
-  maintainSourceHarvesters() {
-    for (const name in Game.rooms) {
-      const room = Game.rooms[name]
-
-      if (room.hasFriendlySpawns()) {
-        for (const source of room.sources) {
-          if (source.harvestablePositionCount > source.harvesters.keys.length) {
-            // Spawn a new harvester
-            // Assign the harvester
-          }
-        }
-      }
-    }
-  }
-
   reclaimDeadMemory() {
     this.reclaimDeadCreepMemory()
     this.reclaimInaccessibleRoomMemory()
@@ -31,16 +16,6 @@ class ForemanPlus {
   reclaimDeadCreepMemory() {
     for (const name in Memory.creeps) {
       if (!Game.creeps[name]) {
-        const creepMemory = Memory.creeps[name]
-
-        if (creepMemory.role === HarvesterCreep.role && creepMemory.target) {
-          const sourceMemory = Memory.sources[creepMemory.target]
-
-          if (sourceMemory && sourceMemory.harvesters[name]) {
-            delete sourceMemory.harvesters[name]
-          }
-        }
-
         debug.log(`Delete dead creep from memory: ${name}`)
         delete Memory.creeps[name]
       }
@@ -50,11 +25,6 @@ class ForemanPlus {
   reclaimInaccessibleRoomMemory() {
     for (const name in Memory.rooms) {
       if (!Game.rooms[name]) {
-        for (const id of Memory.rooms[name].sources) {
-          debug.log(`Delete inaccessible room source from memory: ${name} ${id}`)
-          delete Memory.sources[id]
-        }
-
         debug.log(`Delete inaccessible room from memory: ${name}`)
         delete Memory.rooms[name]
       }
