a17f811a64f3b35a269b6bed3bec9b7377c938ec
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-08-03 17:16:23 -0700

    :memo:

--- dist/room-position-patch.js
+++ dist/room-position-patch.js
@@ -1,3 +1,13 @@
+
+/**
+ * Represents a position within a room.
+ *
+ * @typedef {Object} RoomPosition
+ */
+
+/**
+ * Gets the list of adjacent `RoomPosition` objects.
+ */
 RoomPosition.prototype.getAdjacent = function() {
   const room = Game.rooms[this.roomName]
 
@@ -14,10 +24,20 @@ RoomPosition.prototype.getAdjacent = function() {
   ]
 }
 
+/**
+ * Determines whether the position is walkable.
+ *
+ * A position is walkable if it does not contain a wall.
+ */
 RoomPosition.prototype.isWalkable = function() {
   return this.lookFor(LOOK_TERRAIN)[0] !== "wall"
 }
 
+/**
+ * Determines whether the position is occupied.
+ *
+ * A position is occupied if it contains a creep.
+ */
 RoomPosition.prototype.isOccupied = function() {
   return this.lookFor(LOOK_CREEPS).length != 0
 }
