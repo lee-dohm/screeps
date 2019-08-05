4012a6bf45c8813418b59e39bdad1e456728e22d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-08-04 17:59:02 -0700

    Create builder creeps

--- dist/role-factory.js
+++ dist/role-factory.js
@@ -1,11 +1,16 @@
 "use strict"
 
+const BuilderRole = require("./builder-role")
 const InvalidRoleError = require("./invalid-role-error")
 const HarvesterRole = require("./harvester-role")
 const UpgraderRole = require("./upgrader-role")
 
 function roleFactory(creep) {
   switch (creep.memory.roleId) {
+    case BuilderRole.id: {
+      return new BuilderRole(creep)
+    }
+
     case HarvesterRole.id: {
       return new HarvesterRole(creep)
     }
