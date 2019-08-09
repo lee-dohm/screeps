437ccc108ade65084274dbd46360dd09de27e90b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-08-09 06:49:58 -0700

    Add Road class

---
+++ dist/road.js
@@ -0,0 +1,54 @@
+function getCostMatrix(roomName) {
+  let matrix = new PathFinder.CostMatrix()
+  const room = Game.rooms[roomName]
+
+  room
+    .find(FIND_STRUCTURES, {
+      filter: struct => OBSTACLE_OBJECT_TYPES.includes(struct.structureType)
+    })
+    .map(struct => struct.pos)
+    .forEach(pos => {
+      matrix.set(pos.x, pos.y, 0xff)
+    })
+
+  return matrix
+}
+
+class Road {
+  constructor(a, b) {
+    this.a = this.getPosition(a)
+    this.b = this.getPosition(b)
+  }
+
+  plot() {
+    if (!this.path) {
+      const result = PathFinder.search(
+        this.a,
+        { pos: this.b, range: 1 },
+        { roomCallback: getCostMatrix }
+      )
+
+      this.path = result.path
+    }
+
+    return this.path
+  }
+
+  pave() {
+    if (!this.path) {
+      this.plot()
+    }
+
+    this.path.forEach(pos => {
+      if (pos.lookFor(LOOK_TERRAIN)[0] === "swamp") {
+        pos.createConstructionSite(STRUCTURE_ROAD)
+      }
+    })
+  }
+
+  getPosition(obj) {
+    return obj instanceof RoomPosition ? obj : obj.pos
+  }
+}
+
+module.exports = Road
