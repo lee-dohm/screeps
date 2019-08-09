867126805e6784e19a08365f9e46ef9d3782d66a
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-08-09 07:35:53 -0700

    Use Room.findPath for Road's path finding algorithm

--- dist/road.js
+++ dist/road.js
@@ -1,5 +1,4 @@
-function getCostMatrix(roomName) {
-  let matrix = new PathFinder.CostMatrix()
+function costCallback(roomName, matrix) {
   const room = Game.rooms[roomName]
 
   room
@@ -19,9 +18,7 @@ class Road {
     let road = new Road(obj.a, obj.b)
 
     if (obj.path) {
-      const room = Game.rooms[obj.a.roomName]
-
-      road.path = room.deserializePath(obj.path)
+      road.path = Room.deserializePath(obj.path)
     }
 
     if (obj.paved) {
@@ -38,13 +35,10 @@ class Road {
 
   plot() {
     if (!this.path) {
-      const result = PathFinder.search(
-        this.a,
-        { pos: this.b, range: 1 },
-        { roomCallback: getCostMatrix, swampCost: 1 }
-      )
+      const room = Game.rooms[this.a.roomName]
 
-      this.path = result.path
+      PathFinder.use(true)
+      this.path = room.findPath(this.a, this.b, { ignoreCreeps: true, ignoreRoads: true, costCallback: costCallback, range: 1, swampCost: 1 })
     }
 
     return this.path
@@ -68,7 +62,7 @@ class Road {
     return {
       a: this.a,
       b: this.b,
-      path: this.path ? Game.rooms[this.path[0]].serializePath(this.path) : undefined,
+      path: this.path ? Room.serializePath(this.path) : undefined,
       paved: this.paved
     }
   }
